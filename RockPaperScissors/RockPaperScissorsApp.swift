fee9b48ecfcf121f358ddd9f98a324ce4b53e645
Tonnykh <tonnykhumbongmayum@gmail.com>
2023-06-23 20:13:25 +0530

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Khumbongmayum Tonny on 23/06/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
